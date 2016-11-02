70a1953f857b9f733598162009ae0c75d13c02f7
TriggerAu <dtregoning@outlook.com.au>
2016-11-02 21:52:52 +1100

    Added EditorPartListScrollZDepth for 1.2.1

---
+++ EditorPartListScrollZDepth/EditorPartListScrollZDepth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using KSP;
+
+namespace EditorPartListScrollZDepth
+{
+    [KSPAddon(KSPAddon.Startup.EditorAny,false)]
+    public class EditorPartListScrollZDepth : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("[EditorPartListScrollZDepth]: Searching for EditorPartList");
+            if(KSP.UI.Screens.EditorPartList.Instance != null) {
+                Debug.Log("[EditorPartListScrollZDepth]: Found It. Now get the scrollbar handle");
+                Transform editorPartListScrollHandle = KSP.UI.Screens.EditorPartList.Instance.gameObject.transform.Find("Mode Transition/PartList Area/PartList and sorting/ListAndScrollbar/Scrollbar/Sliding Area/Handle");
+
+                if(editorPartListScrollHandle != null) {
+                    Debug.Log("[EditorPartListScrollZDepth]: Position was: " + editorPartListScrollHandle.localPosition);
+                    editorPartListScrollHandle.localPosition = new Vector3(editorPartListScrollHandle.localPosition.x, editorPartListScrollHandle.localPosition.y, -1f);
+                    Debug.Log("[EditorPartListScrollZDepth]: Position now: " + editorPartListScrollHandle.localPosition);
+                } else {
+                    Debug.Log("[EditorPartListScrollZDepth]: Couldnt get handle.");
+                }
+            } else {
+                Debug.Log("[EditorPartListScrollZDepth]: Couldnt find it.");
+            }
+            Debug.Log("[EditorPartListScrollZDepth]: Done");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,5 +2,13 @@ KSPFixes
 ================
 Some plugin fixes for issues in stock [Kerbal Space Program](http://www.kerbalspaceprogram.com/)
 
+----
+
+### EditorPartListScrollZDepth
+Fix for scrollist handle disappearing if a large number of plugins/mods installed
+```KSPVersion: 1.2.1```
+
+----
+
 Forum Thread: [KerbalAlarmClock](http://forum.kerbalspaceprogram.com/threads/24786-Kerbal-Alarm-Clock)
 Author: TriggerAu
