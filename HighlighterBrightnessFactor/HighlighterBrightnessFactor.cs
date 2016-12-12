5a1da9827699e88191d0f74644dcf93eba2f337d
TriggerAu <dtregoning@outlook.com.au>
2016-12-12 22:10:21 +1100

    Fix for HighlighterBrightnessFactor not being applied on gameload
    Relates to http://bugs.kerbalspaceprogram.com/issues/13399

---
+++ HighlighterBrightnessFactor/HighlighterBrightnessFactor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using KSP;
+
+namespace HighlighterBrightnessFactor
+{
+    [KSPAddon(KSPAddon.Startup.FlightAndEditor,false)]
+    public class HighlighterBrightnessFactor : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("[HighlighterBrightnessFactor]: Setting highlighterlimit from gamesettings");
+
+            Highlighting.Highlighter.HighlighterLimit = GameSettings.PART_HIGHLIGHTER_BRIGHTNESSFACTOR;
+
+            Debug.Log("[HighlighterBrightnessFactor]: Done");
+        }
+    }
+}
